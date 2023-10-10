82b5e5c53d993d168721371ca0f30c3be801f764
nicholasladika <129184657+nicholasladika@users.noreply.github.com>
2023-10-10 10:19:36 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,24 @@
+const calcular = document.getElementById('calcular');
+
+function imc () {
+    const nome =document.getElementById('nome').value;
+    const altura = +document.getElementById('altura').value;
+    const peso = +document.getElementById('peso');value
+    const resultado = document.getElementById('resultado');
+
+     if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC <18.5){
+            classificacao = 'abaixodo peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal.Parabéns!!!';
+        }else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso.';
+        }
+     }
+
+}
